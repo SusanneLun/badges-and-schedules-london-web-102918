27c316cbfed1bade00d532ba72ba84eae12ad06a
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2018-10-25 08:45:06 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name)
+  loop do
+    name
+puts "Hello, my name is #{name}."
+end
+end
+
+
